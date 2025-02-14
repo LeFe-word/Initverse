963b6f5f92e421c8b28f492da3dff5a75ba6042a
LeFe <142906595+LeFe-word@users.noreply.github.com>
2025-02-14 13:51:20 +0300

    Create ins_init.sh

---
+++ ins_init.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+apt install nano
+
+mkdir -p $HOME/initverse
+cd $HOME/initverse
+wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/iniminer-linux-x64
+chmod +x iniminer-linux-x64
+cd
