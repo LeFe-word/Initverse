ecf7710ef3a07bb5b19c57a779dfbf5b8ee45efc
LeFe <142906595+LeFe-word@users.noreply.github.com>
2025-02-19 11:22:24 +0300

    Update initv.sh

--- initv.sh
+++ initv.sh
@@ -15,11 +15,11 @@ fi
 # Формируем новую строку с --cpu-devices
 NEW_CPU_DEVICES=""
 for ((i=1; i<=CPU_COUNT; i++)); do
-  NEW_CPU_DEVICES+=" --cpu-devices $i "
+  NEW_CPU_DEVICES+=" --cpu-devices $i"
 done
 
 # Используем sed для замены только части с --cpu-devices
-sudo sed -i -E "s|(ExecStart=/root/initverse/iniminer-linux-x64 --pool stratum\+tcp://[^ ]+)( --cpu-devices [0-9 ]+)|\\1$NEW_CPU_DEVICES|" "$SERVICE_FILE"
+sudo sed -i -E "s|(ExecStart=/root/initverse/iniminer-linux-x64 --pool stratum\+tcp://[^ ]+)|\\1$NEW_CPU_DEVICES|" "$SERVICE_FILE"
 
 # Перезагружаем systemd, чтобы применить изменения
 sudo systemctl daemon-reload
@@ -28,3 +28,4 @@ sudo systemctl daemon-reload
 sudo systemctl restart initverse-miner
 
 echo "Количество --cpu-devices изменено на: $NEW_CPU_DEVICES"
+journalctl -u initverse-miner -n 25 -f --no-hostname
